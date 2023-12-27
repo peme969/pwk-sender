9afff507b829752d54098bb14e554d55ff37a1ab
Peme969 ⚡ <136040410+peme969@users.noreply.github.com>
2023-12-27 13:21:13 -0600

    Create logo.py

---
+++ logo.py
@@ -0,0 +1,61 @@
+import os
+import time
+
+from termcolor import colored
+
+print(colored(
+  """
+  
+
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@80GG0GG00000000000000000000000000000GGG000G0G08@88888888888888888888888888888888888888@@@@@@@@@
+@@@@GLffffffffffffffffffffffffffffffffffffffffffftL88888888888888888888888888888888888888888888@@@@@
+@@@@CffffffffffffffffffffffffffffffffffffffffffftfG888888888888888888888888888888888888888888888@@@@
+@@@@CffffffffffffffffffffffffffffffffffffffffffftL88888888888888888888888888888888888888888888888@@@
+@@@@CfffffffffffffffffffffffffffffffffffffffffftfG88888888888888888888888888888888888888888888888@@@
+@@@@CfffffffffffffffffffffffffffffffffffffffffftL088888888888888888888888888888888888888888888888@@@
+@@@@CfffffffffffffffffffffffffffffffffffffffffftC888888888888888888888888888888888888888888888888@@@
+@@@@Cffffffffffffffffffffffffffffffffffffffffftf0888888888888888888888888888888888888888888888888@@@
+@@@@CffffffffffffffffffffffffffffffffffffffffftC8888888888888888888888888888888888888888888888888@@@
+@@@@CffffffffffffffffffffffffffffffffffffffffttG8888888888888888888888888888888888888888888888888@@@
+@@@@Cfffffffffffffffffffffffffffffffffffffffftf08888888888888888888888888888888888888888888888888@@@
+@@@@CfffffffffffffffftffffffttffffffffffffffftC88888888888888888888880G08888888888888888888888888@@@
+@@@@Cfffffffffffffffffffff1:,:1fffffffffffffffG888888888888888888888L:,:L888888888888888888888888@@@
+@@@@CfffffffffffffffffffLf:...;tfffffffffffftL0888888888888888888880i . ;0@8888888888888888888888@@@
+@@@@Cfffffffffffffffffffft:...;tfffffffffffftG8888888888888888888880i   ;088888888888888888888888@@@
+@@@@Cfffffffffffffffffffff:...;tfffffffffffft08888888888888888888880i . ;088888888888888888888888@@@
+@@@@Cfffffffffffffffffffff:...ifffffffffffftL888888888888888888888801. .1088888888888888888888888@@@
+@@@@Cfffffffffffffffffffffti;itfffffffffffftG888888888888888888888880ftf0888888888888888888888888@@@
+@@@@Cffffffffffffffffffffffffffffffffffffftt08888888888888888888888888888888888888888888888888888@@@
+@@@@Cffffffffffffffffffffffffffffffffffffftf08888888888888888888888888888888888888888888888888888@@@
+@@@@CffffffffffffffffffffffffffffffffffffftL88888888888888888888888888888888888888888888888888888@@@
+@@@@CffffffffffffffffffffffffffffffffffffftC88888888888888888888888888888888888888888888888888888@@@
+@@@@CffffffffffffffffffffffffffffffffffffftG88888888888888888888888888888888888888888888888888888@@@
+@@@@CffffffffffffffffffffffffffffffffffffftG88888888888888888888888888888888888888888888888888888@@@
+@@@@CffffffffffffffffffffffffffffffffffffftG88888888888888888888888888888888888888888888888888888@@@
+@@@@CffffffffffffffffffffffffffffffffffffffL08888888888888888888888888888888888888888888888888888@@@
+@@@@CffffffffffffffffffffffffffffffffffffffffLLLLLLLLLLLL8888888888888888888888888888888888888888@@@
+@@@@CfffffffffffffffffffffffffffffffffffffffffffffffffftL8888888888888888888888888888888888888888@@@
+@@@@CfffffffffffffffffffffffffffffffffffffffffffffffffftC888888888888888888888@888888888888888888@@@
+@@@@Cfffffffffffffffff1;i1ffffffffffffffffffffffffffffftG88888888888888888@0L11L88888888888888888@@@
+@@@@Cffffffffffffffff1:..,:itfffffffffffffffffffffffffftG88888888888888@8Gf:,  108888888888888888@@@
+@@@@Cfffffffffffffffffti:,...:;1ffffffffffffffffffffffttG8888888888880Gf;,. ,iL088888888888888888@@@
+@@@@Cffffffffffffffffffffti:....,;i1tfffffffffffffffffftC888888888GLt;,  .;fG88888888888888888888@@@
+@@@@Cfffffffffffffffffffffffti;,....,:;i1ttfffffffffffffC8880GCf1;,. .,;tG88888888888888888888888@@@
+@@@@Cfffffffffffffffffffffffffftti;,.....,,::;;iiiiiiiiii1;::.   .:;tCG88888888888888888888888888@@@
+@@@@Cffffffffffffffffffffffffffffffft11;::,,....    .......,::itLC0888888888888888888888888888888@@@
+@@@@Cfffffffffffffffffffffffffffffffffffffttt11iiiiiiiiiiLCG0088888888888888888888888888888888888@@@
+@@@@CffffffffffffffffffffffffffffffffffffffffffffffffffftG888888888888888888888888888888888888888@@@
+@@@@CffffffffffffffffffffffffffffffffffffffffffffffffffftC088888888888888888888888888888888888888@@@
+@@@@Cffffffffffffffffffffffffffffffffffffffffffffffffffftf088888888888888888888888888888888888888@@@
+@@@@CfffffffffffffffffffffffffffffffffffffffffffffffffffffG88888888888888888888888888888888888888@@@
+@@@@CfffffffffffffffffffffffffffffffffffffffffffffffffffftC88888888888888888888888888888888888888@@@
+@@@@Cfffffffffffffffffffffffffffffffffffffffffffffffffffftf08888888888888888888888888888888888888@@@
+@@@@CtfffffffffffffffffffffffffffffffffffffffffffffffffffftC8888888888888888888888888888888888888@@@
+@@@@8CLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLfC8888888888888888888888888888888888888@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+Created by Peme969 ⚡
+Check out github profile: https://github.com/peme969/
+""", 'green'))
